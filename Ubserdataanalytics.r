c2c02bf84254e408f6ab6fe4eb40fb24ca247f9e
Rajeshwari-17 <119601763+Rajeshwari-17@users.noreply.github.com>
2023-09-07 11:45:08 +0530

    Update Ubserdataanalytics.r

--- Ubserdataanalytics.r
+++ Ubserdataanalytics.r
@@ -1,1 +1,151 @@
+#Uber Data Analysis through Visualisations in R
+#Data storytelling is an important component of Machine Learning through which companies are able to understand the background of various operations. 
+#With the help of visualization, companies can avail the benefit of understanding the complex data and gain insights that would help them to craft decisions. 
+#This is more of a data visualization project that will guide you towards using the ggplot2 library for understanding the data and for developing an intuition for understanding the customers who avail the trips.
+#Important: The goal of this project is to learn visualisations in R.
+
+#Importing necessary libraries
+#gplot2: ggplot2 is the most popular data visualization library that is most widely used for creating aesthetic visualization plots.
+#lubridate: Use time-frames in the dataset
+#dplyr: Data Manipulation
+#tidyr: Tidy the data
+#DT: Datatables in JS
+#scales: With the help of graphical scales, we can automatically map the data to the correct scales with well-placed axes and legends.
+
+library(ggplot2)
+library(ggthemes)
+library(lubridate)
+library(dplyr)
+library(tidyr)
+library(tidyverse) # metapackage of all tidyverse packages
+library(DT)
+library(scales)
+
+# Read the data for each month separately 
+apr <- read.csv("../input/uberdataset/uber-raw-data-apr14.csv")
+may <- read.csv("../input/uberdataset/uber-raw-data-may14.csv")
+june <- read.csv("../input/uberdataset/uber-raw-data-jun14.csv")
+july <- read.csv("../input/uberdataset/uber-raw-data-jul14.csv")
+aug <- read.csv("../input/uberdataset/uber-raw-data-aug14.csv")
+sept <- read.csv("../input/uberdataset/uber-raw-data-sep14.csv")
+
+# Combine the data together 
+data <- rbind(apr, may, june, july, aug, sept)
+cat("The dimensions of the data are:", dim(data))
+
+#The data contains the columns Date.Time which is a factor, Latitude and Longitudes which are double and Base which is factor. 
+#we will format the datetime into a more readable format using the Date Time conversion function.
+
+data$Date.Time <- as.POSIXct(data$Date.Time, format="%m/%d/%Y %H:%M:%S")
+data$Time <- format(as.POSIXct(data$Date.Time, format = "%m/%d/%Y %H:%M:%S"), format="%H:%M:%S")
+data$Date.Time <- ymd_hms(data$Date.Time)
+
+# Create individual columns for month day and year
+data$day <- factor(day(data$Date.Time))
+data$month <- factor(month(data$Date.Time, label=TRUE))
+data$year <- factor(year(data$Date.Time))
+data$dayofweek <- factor(wday(data$Date.Time, label=TRUE))
+
+# Add Time variables as well 
+data$second = factor(second(hms(data$Time)))
+data$minute = factor(minute(hms(data$Time)))
+data$hour = factor(hour(hms(data$Time)))
+
+#Data Visualisation
+Data Visualisation
+Plotting the trips by hours in a day
+hourly_data <- data %>% 
+                    group_by(hour) %>% 
+                            dplyr::summarize(Total = n())
+
+# Shos data in a searchable js table
+datatable(hourly_data)
+
+# Plot the data by hour
+ggplot(hourly_data, aes(hour, Total)) + 
+geom_bar(stat="identity", 
+         fill="steelblue", 
+         color="red") + 
+ggtitle("Trips Every Hour", subtitle = "aggregated today") + 
+theme(legend.position = "none", 
+      plot.title = element_text(hjust = 0.5), 
+      plot.subtitle = element_text(hjust = 0.5)) + 
+scale_y_continuous(labels=comma)
+
+# Aggregate the data by month and hour
+month_hour_data <- data %>% group_by(month, hour) %>%  dplyr::summarize(Total = n())
+
+ggplot(month_hour_data, aes(hour, Total, fill=month)) + 
+geom_bar(stat = "identity") + 
+ggtitle("Trips by Hour and Month") + 
+scale_y_continuous(labels = comma)
+
+# Aggregate data by day of the month 
+day_data <- data %>% group_by(day) %>% dplyr::summarize(Trips = n())
+day_data
+
+# Plot the data for the day
+ggplot(day_data, aes(day, Trips)) + 
+geom_bar(stat = "identity", fill = "steelblue") +
+ggtitle("Trips by day of the month") + 
+theme(legend.position = "none") + 
+scale_y_continuous(labels = comma)
+
+# Collect data by day of the week and month
+
+day_month_data <- data %>% group_by(dayofweek, month) %>% dplyr::summarize(Trips = n())
+day_month_data
+
+# Plot the above data
+ggplot(day_month_data, aes(dayofweek, Trips, fill = month)) + 
+geom_bar(stat = "identity", aes(fill = month), position = "dodge") + 
+ggtitle("Trias by Day and Month") + 
+scale_y_continuous(labels = comma) + 
+scale_fill_manual(values = colors)
+
+month_data <- data %>% group_by(month) %>% dplyr::summarize(Total = n())
+
+month_data
+
+ggplot(month_data, aes(month, Total, fill = month)) + 
+geom_bar(stat = "Identity") + 
+ggtitle("Trips in a month") + 
+theme(legend.position = "none") + 
+scale_y_continuous(labels = comma) + 
+scale_fill_manual(values = colors)
+day_hour_data <- data %>% group_by(day, hour) %>% dplyr::summarize(Total = n())
+datatable(day_hour_data)
+ggplot(day_hour_data, aes(day, hour, fill = Total)) + 
+geom_tile(color = "white") + 
+ggtitle("Heat Map by Hour and Day")
+
+# Collect data by month and day
+
+month_day_data <- data %>% group_by(month, day) %>% dplyr::summarize(Trips = n())
+month_day_data
+
+# Plot a heatmap 
+
+ggplot(month_day_data, aes(day, month, fill = Trips)) + 
+geom_tile(color = "white") + 
+ggtitle("Heat Map by Month and Day")
+
+# Plot a heatmap by day of the week and month
+
+ggplot(day_month_data, aes(dayofweek, month, fill = Trips)) + 
+geom_tile(color = "white") + 
+ggtitle("Heat Map by Month and Day")
+
+# Set Map Constants
+min_lat <- 40 
+max_lat <- 40.91
+min_long <- -74.15
+max_long <- -73.7004
+ggplot(data, aes(x=Lon, y=Lat)) +
+  geom_point(size=1, color = "blue") +
+     scale_x_continuous(limits=c(min_long, max_long)) +
+      scale_y_continuous(limits=c(min_lat, max_lat)) +
+        theme_map() +
+           ggtitle("NYC MAP BASED ON UBER RIDES DURING 2014 (APR-SEP)")
+
 
